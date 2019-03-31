8956f34a1df74891e2ea8068adf8ffd1b269ef73
BSalim <burhan.salman@gmail.com>
2019-03-31 02:37:11 +0100

    add slide rotate and fall to actions

--- actions.js
+++ actions.js
@@ -8,3 +8,52 @@ function elementToKick(x,y,threshold=4000) {
     else viable = element;
   });
 }
+
+function slideOff(element,orientation=1) {
+  let i = 0;
+  let initial = element.getBoundingClientRect().left;
+  let done = false;
+  if (orientation== 1) {
+    return setInterval(()=>{
+      if (initial + i < window.innerWidth) {
+        element.style.transform = "translateX("+i+"px)";
+        i += 16;
+      } else if (!done) {
+        done = true;
+        element.parentNode.removeChild(element);
+      }
+    }, 2);
+  } else if (orientation == -1) {
+    initial = element.getBoundingClientRect().right;
+    return setInterval(()=>{
+      if (initial - i > 0) {
+        element.style.transform = "translateX(-"+i+"px)";
+        i += 16;
+      } else if (!done) {
+        done = true;
+        element.parentNode.removeChild(element);
+      }
+    }, 2);
+  }
+}
+
+function rotateSmall(element) {
+  element.style.transform = "rotate(10deg)";
+}
+
+function fallDown(element) {
+  element.style.position = "fixed";
+  let rect = element.getBoundingClientRect();
+  let done = false;
+  let i = 0;
+  function fall() {
+    if (rect.bottom + i < innerHeight) {
+      element.style.transform = "translateY("+i+"px)";
+      i += 10;
+    } else {
+      done = true;
+    }
+    if (!done) requestAnimationFrame(fall);
+  }
+  requestAnimationFrame(fall);
+}
